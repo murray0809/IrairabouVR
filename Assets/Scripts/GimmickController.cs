fd4f8bb7f89a0a225c85143c7fe5819b8cf5a0b7
AraiKanta <akgame36@gmail.com>
2021-11-06 15:29:25 +0900

    回転するようにした

--- Assets/Scripts/GimmickController.cs
+++ Assets/Scripts/GimmickController.cs
@@ -7,9 +7,30 @@ public class GimmickController : MonoBehaviour
     /// <summary> 回転させるための値の変数 </summary>
     [Header("回転させるための値")]
     [SerializeField] private Vector3 rotationVec;
+    /// <summary> 移動スピードの変数 </summary>
+    [Header("移動スピード")]
+    [SerializeField] private float moveSpeed;
+
+    [Header("テスト用")]
+    public bool isMove = false;
 
     void Update()
     {
+        //回転
         transform.Rotate(new Vector3(rotationVec.x, rotationVec.y, rotationVec.z));
+
+        if (isMove)
+        {
+            gameObject.transform.position += transform.position * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    //TODO:後日やる
+    /// <summary>
+    /// 移動を制限する関数
+    /// </summary>
+    private void GimmickMove()
+    {
+        
     }
 }
